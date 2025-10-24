58c0db64b042e4fc33ed6adecbd7dd8968848a6b
Yasmin <yasminxismail@gmail.com>
2025-10-25 00:15:43 +0100

    Setup express app, environment variables for port, middleware logging out the requests coming in (path, method).

--- MernStack/backend/server.js
+++ MernStack/backend/server.js
@@ -2,22 +2,22 @@
 require('dotenv').config(); // invoke config() - going to attach those envir vars to the process object
 // register the express app
 const express = require('express');
+
+const workoutRoutes = require('./routes/workouts');
  
 // starts up express app 
 const app = express(); //  invoke function
 
 //middleware
+app.use(express.json()); // any request that comes in, it looks if it has some body to the request, some data that's sending to the server
+                        // if it does, it passes it & attaches it to the request object so it can be accessed in the request handler
 app.use((req,res,next) =>{ // logs out the requests coming in
     console.log("req path: " + req.path + " req method:" + req.method);
     next();
 }) // have to run function next in order to move onto the next middleware (e.g ap..get('/'))
 
 // routes
-// react to requests - set up a route handler
-app.get('/', (req,res) => {
-    console.log('GET / request received');
-    res.json({mssg: 'Welcome to the app'}) // sends json string, property called mssg
-}) // once it goes into domain, then fire a function which is gonna handle that request
+app.use('/api/workouts', workoutRoutes); //  when this reuqest is fired, use routes in workoutRoutes
 
 // listen for requests
 app.listen(process.env.PORT, () => {
